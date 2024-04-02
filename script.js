dbbdb10ff17862b2c74fe98148f7eb333562e2b0
FaiqaAbbas <faiqaabbas474@gmail.com>
2024-04-02 17:46:52 +0500

    uploaded

---
+++ script.js
@@ -0,0 +1,14 @@
+var on_btn=document.querySelector('.on-btn');
+var off_btn=document.querySelector('.off-btn');
+var bulb_image=document.querySelector('.bulb_image');
+
+on_btn.addEventListener('click',function(){
+    // bulb_image.src="./bulb_on.png";
+    document.body.style.background= "radial-gradient(circle, rgba(255, 215, 0, 0.8) 30%, rgba(255, 153, 0, 0.6) 50%, rgba(255, 208, 0, 0.144) 70%)";
+
+
+});
+off_btn.addEventListener('click',function(){
+    bulb_image.src="./bulb_off.png";
+    document.body.style.background="black";
+});
