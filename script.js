3c67dfe594a589bcbbc8c68724964f477c9ed03d
FaiqaAbbas <134505855+FaiqaAbbas@users.noreply.github.com>
2024-04-02 18:33:03 +0500

    Update script.js

--- script.js
+++ script.js
@@ -6,7 +6,6 @@ on_btn.addEventListener('click',function(){
     // bulb_image.src="images/bulb_on.png";
     document.body.style.background= "radial-gradient(circle, rgba(255, 215, 0, 0.8) 30%, rgba(255, 153, 0, 0.6) 50%, rgba(255, 208, 0, 0.144) 70%)";
 
-
 });
 off_btn.addEventListener('click',function(){
     bulb_image.src="images/bulb_off.png";
